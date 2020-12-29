e96e63e581e6acf52fe9b12591f78828761d8f70
peter <peter.bogar@pandora.sk>
2020-12-29 22:00:52 +0100

    finish output table: price, shares for each symbol, for each day

--- wp_backtest.py
+++ wp_backtest.py
@@ -1,20 +1,20 @@
-# Vstup fnkcie bude zoznam symbolov a perioda v rokoch, perioda ATR
-# nacita CSV pre kazdy symbol za dany pocet rokov
-# Z tohto CSV bude potom vyberat hodnoty raz za mesiac za dany pocet dni- prerioda ATR
+# Vstup fnkcie bude zoznam symbolov a perioda v rokoch, perioda ATR v dnoch
 # novy pocet akci porovna so starym, a vypocita zisk
 # vystup bude tabulka- jeden riadok-jeden mesiacx pocet , kde bude cena
 
 import pandas as pd
-import pandas_datareader as pd_web
-from datetime import date, datetime, timedelta
+# import pandas_datareader as pd_web
+from datetime import date
+import weight_portfolio
 
 # Symbols to do backtest on
-symbols = ['SPY', 'GLD']
+symbols = ['SPY', 'GLD', 'AAPL']
 # Time period in YEARS how long in history to do backtest,
 # Last year is current- even if not finished yet
-test_period_y = 2
+test_period_y = 1
 # Time period in DAYS for ATR
 atr_period = 20
+account = 10000
 
 current_year = date.today().year
 test_begin = current_year - test_period_y + 1
@@ -22,6 +22,7 @@ test_end = current_year + 1
 
 # Empty variables
 data_all = pd.DataFrame()
+collected_data = pd.DataFrame()
 
 print()
 
@@ -39,14 +40,33 @@ print()
 #         # print(select_date)
 
 for symbol in symbols:
-    # Load data for each symbol in the list
-    print('Loading ', symbol)
-    data = pd_web.DataReader(symbol, 'yahoo', test_begin, test_end)
-    data_all[symbol] = data['Adj Close']
+    downloaded_data = weight_portfolio.data_download_for_backtest(symbol, test_begin, test_end)
+    atr_data = weight_portfolio.count_atr(downloaded_data, atr_period)
 
-# cyklus for pre kazdy symbol
-#   nacitat x rokov
-#   v cykle pre kazdy mesiac, nacitat
-#       vypocet atr, vaha, pocet
-#       vystup: nova tabulka- kazdy mesiac: cena, pocet akcii
-#   doplnit do dalsie stlpce do existujucej tabulky
+    collected_data['Close_'+symbol] = downloaded_data['Close']
+    collected_data['ATR_'+symbol] = atr_data['ATR']
+
+collected_data['ATR_sum'] = collected_data.filter(regex='ATR').sum(axis=1)
+
+for symbol in symbols:
+    # Calculation weight, formula= (1-ATR/Sum of ATRs)/(number of symbols -1)
+    collected_data['Weight_'+symbol] = ((1 - collected_data['ATR_'+symbol] / collected_data['ATR_sum']) / (len(symbols) - 1)).round(3)
+    # Number of shares based on weight, current price and account
+    collected_data['Shares_'+symbol] = (collected_data['Weight_'+symbol] * account / collected_data['Close_'+symbol]).round(0)
+
+
+output_data = collected_data.filter(regex='Close|Shares')
+print(output_data)
+
+# for y in range(test_begin, test_end):
+#     for m in range(1, 13):
+#         # Last day in month must be 28th because of February
+#         select_date = datetime(y, m, 28)
+#
+#         # If selected date is weekend, decrease the date, check it 4x
+#         for i in [1, 2, 3, 4]:
+#             try:
+#                 downloaded_data.loc[select_date]
+#             except KeyError:
+#                 select_date = select_date - timedelta(days=1)
+#         # print(select_date)
