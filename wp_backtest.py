e6c6c9bb413ecc70da93dae3254a393fb8d492fa
peter <peter.bogar@pandora.sk>
2020-12-29 15:03:17 +0100

    correctly choose select date, it exist in the dataframe

--- wp_backtest.py
+++ wp_backtest.py
@@ -26,25 +26,20 @@ data_all = pd.DataFrame()
 print()
 
 for symbol in symbols:
+    # Load data for each symbol in the list
     print('Loading ', symbol)
     data = pd_web.DataReader(symbol, 'yahoo', test_begin, test_end)
     data_all[symbol] = data['Adj Close']
 
-# print(data_all)
-
-select_date = datetime(2020, 12, 24)
-
-# If selected date is weekend, decrease the date- check it 3x
-# for i in [1, 2, 3]:
-#     try:
-#         data_all.loc[select_date]
-#     except KeyError:
-#         select_date = select_date - timedelta(days=1)
-#
-# print(data_all.loc[select_date])
-
 for y in range(test_begin, test_end):
     for m in range(1, 13):
         # Last day in month must be 28th because of February
-        select_date = date(y, m, 28)
+        select_date = datetime(y, m, 28)
+
+        # If selected date is weekend, decrease the date- check it 3x
+        for i in [1, 2, 3, 4]:
+            try:
+                data_all.loc[select_date]
+            except KeyError:
+                select_date = select_date - timedelta(days=1)
         print(select_date)
