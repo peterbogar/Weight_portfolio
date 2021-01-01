a8d127b0ed51b8c39702859399af5c92d5aed5ca
peter <peter.bogar@pandora.sk>
2021-01-01 20:30:30 +0100

    check in excel is OK output dataframe is profit for each symbol

--- wp_backtest.py
+++ wp_backtest.py
@@ -1,15 +1,17 @@
 # Vstup fnkcie bude zoznam symbolov, perioda v rokoch, perioda ATR v dnoch
+# Vystup bude tabulka so zvolenou testovacou periodov (teraz koniec mesiaca)
+# A profitu pre kazdy symbol
 
 import pandas as pd
 from datetime import datetime, date, timedelta
 import weight_portfolio
 
 # Symbols to do backtest on
-symbols = ['SPY', 'GLD']
+symbols = ['SPY', 'GLD', 'AAPL']
 
 # Time period in YEARS how long in history to do backtest
 # Last year is current- even if not finished yet
-test_period_y = 1
+test_period_y = 2
 
 # Time period in DAYS for ATR
 atr_period = 20
@@ -18,6 +20,7 @@ account = 10000
 
 
 def count_avg_price(fdiff, fclose_price, fshares, fprevious_shares, fprevious_avg_price):
+    # Funkicia pocita average cenu
     # Variables starts with F because of warning message
     # ak je dif vacsi ako nula
     # (cena * roziel + pred suma akci * pred avg cena) / nova suma akci
@@ -31,18 +34,19 @@ def count_avg_price(fdiff, fclose_price, fshares, fprevious_shares, fprevious_av
 
 
 def count_profit(fdiff, fclose_price, favg_price):
+    # Funkcia pocita profit
     # Variables starts with F because of warning message
     # ak je rozdiel akci mensi ako nula
     # (avg cena-cena) * rozdiel
     # ak je rozdiel vacsi ako nula
     # 0
-    if fdiff > 0:
+    if fdiff < 0:
         profit = (favg_price-fclose_price)*fdiff
         return profit
     else:
         return 0
 
-
+# Set up start and end testing period
 current_year = date.today().year
 test_begin = current_year - test_period_y
 test_end = current_year
@@ -54,6 +58,7 @@ pd.set_option('display.max_columns', None)
 close_atr_data = pd.DataFrame()
 selecet_date_data = pd.DataFrame({'Date': []})
 print()
+
 
 # Download data and count ATR for each symbol
 for symbol in symbols:
@@ -77,6 +82,8 @@ for symbol in symbols:
 # Hide unnecessary columns
 close_shares_data = close_atr_data.filter(regex='Close|Shares')
 
+# TODO: prerobit na modifikovatelne
+# Time period for rebalancing, currently once a month
 # Pick up one day in each month, create new dataframe with just Date-index
 for y in range(test_begin, test_end):
     for m in range(1, 13):
@@ -90,6 +97,8 @@ for y in range(test_begin, test_end):
             except KeyError:
                 select_date = select_date - timedelta(days=1)
         selecet_date_data.loc[len(selecet_date_data.index)] = [select_date]
+
+# Set index
 selecet_date_data = selecet_date_data.set_index('Date')
 
 # Combine both dataframes, with all data and with just picked up days
@@ -97,38 +106,42 @@ close_shares_selected_date_data = pd.merge(selecet_date_data, close_shares_data,
 
 # Added columns for previous shares number and difference in shares
 for symbol in symbols:
-    close_shares_selected_date_data['Prev sh_'+symbol] = close_shares_selected_date_data['Shares_'+symbol].shift(1)
-    close_shares_selected_date_data['Diff sh_'+symbol] = close_shares_selected_date_data['Shares_'+symbol] - close_shares_selected_date_data['Prev sh_'+symbol]
+    close_shares_selected_date_data['Prev shares'+symbol] = close_shares_selected_date_data['Shares_'+symbol].shift(1)
+    close_shares_selected_date_data['Diff shares'+symbol] = close_shares_selected_date_data['Shares_'+symbol] - close_shares_selected_date_data['Prev shares'+symbol]
 
-# Create column for average price and profit for each symbol
+# Create empty column for average price and profit for each symbol
 for symbol in symbols:
-    close_shares_selected_date_data['Avg Close_'+symbol] = None
+    close_shares_selected_date_data['Avg price_'+symbol] = None
     close_shares_selected_date_data['Profit_' + symbol] = None
 
-# First average price is close price for each symbol
+# Set first average price is close price for each symbol
 for symbol in symbols:
-    close_shares_selected_date_data.iloc[0, close_shares_selected_date_data.columns.get_loc('Avg Close_'+symbol)] = close_shares_selected_date_data.iloc[0, close_shares_selected_date_data.columns.get_loc('Close_'+symbol)]
+    close_shares_selected_date_data.iloc[0, close_shares_selected_date_data.columns.get_loc('Avg price_'+symbol)] = close_shares_selected_date_data.iloc[0, close_shares_selected_date_data.columns.get_loc('Close_'+symbol)]
 
-
+# How many rebalancing are in the testing period
 number_of_days = range(0, len(close_shares_selected_date_data.index))
 
+# For each symbol count average price and profit in separate functions
 for symbol in symbols:
     for index in number_of_days:
         if index == 0:
             pass
         else:
-            # Set variables for function 'count average price'
-            diff = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Diff sh_'+symbol)]
+            # Set variables for function 'count average price' and 'count profit'
+            diff = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Diff shares'+symbol)]
             close_price = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Close_'+symbol)]
             shares = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Shares_'+symbol)]
-            previous_shares = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Prev sh_'+symbol)]
-            previous_avg_price = close_shares_selected_date_data.iloc[index-1, close_shares_selected_date_data.columns.get_loc('Avg Close_'+symbol)]
+            previous_shares = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Prev shares'+symbol)]
+            previous_avg_price = close_shares_selected_date_data.iloc[index-1, close_shares_selected_date_data.columns.get_loc('Avg price_'+symbol)]
 
-            # Count average price
-            close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Avg Close_'+symbol)] = count_avg_price(diff, close_price, shares, previous_shares, previous_avg_price)
+            # Count average price in function
+            close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Avg price_'+symbol)] = count_avg_price(diff, close_price, shares, previous_shares, previous_avg_price)
 
-            avg_price = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Avg Close_'+symbol)]
-            # Count profit
+            # Count profit in function
+            avg_price = close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Avg price_'+symbol)]
             close_shares_selected_date_data.iloc[index, close_shares_selected_date_data.columns.get_loc('Profit_' + symbol)] = count_profit(diff, close_price, avg_price)
 
-print(close_shares_selected_date_data)
+# Choose columns to be in output dataframe
+# output_data = close_shares_selected_date_data.filter(regex='Shares|Profit')
+output_data = close_shares_selected_date_data.filter(regex='Profit')
+print(output_data)
