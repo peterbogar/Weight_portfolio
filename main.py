0f2d41e6549cccba51d4ed480cf6ee041157c94e
peter <peter.bogar@pandora.sk>
2021-01-03 13:26:37 +0100

    drawdown for each symbol

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import pandas as pd
 from datetime import date, datetime, timedelta
 
 # List of symbols you want to use
-symbols = ['SPY', 'GLD']
+symbols = ['SPY', 'XLE']
 
 # Time period in days for ATR indicator
-atr_time_period = 20
+atr_time_period = 2
 
 # Time period in years to backtest
-test_period_y = 2
+test_period_y = 10
 
 # Sum of account in USD
 account = 10000
@@ -144,9 +144,83 @@ def backtest_atr_weight(fsymbols, ftest_period_y, fatr_time_period, faccount):
 
 
 def backtest_summary(fdata):
+    # Summarize cumulative profit among all symbols
     cum_profit_data = fdata.filter(regex='Cum').copy()
     cum_profit_data['Sum profit'] = cum_profit_data.sum(axis=1)
-    # cum_profit_data.loc['Summary', :] = cum_profit_data.cumsum()
+
+    # Numbers of days for backtest (index)
+    number_of_days = range(0, len(cum_profit_data.index))
+
+    # Add new column Peak and Drawdown for each symbol, first row is 0
+    for symbol in symbols:
+        cum_profit_data.loc[cum_profit_data.index[0], 'Peak_'+symbol] = 0
+        cum_profit_data.loc[cum_profit_data.index[0], 'DD%_' + symbol] = 0
+
+    # Peak in profit for symbols
+    for symbol in symbols:
+        # Column- symbol
+        column_profit = cum_profit_data.columns.get_loc('Cum_profit_'+symbol)
+        column_peak = cum_profit_data.columns.get_loc('Peak_'+symbol)
+        column_dd = cum_profit_data.columns.get_loc('DD%_'+symbol)
+
+        # Row- index
+        for row in number_of_days:
+            curr_profit = cum_profit_data.iloc[row, column_profit]
+            curr_peak = cum_profit_data.iloc[row, column_peak]
+            prev_peak = cum_profit_data.iloc[row - 1, column_peak]
+            # Skip first row
+            if row == 0:
+                pass
+            elif curr_profit > prev_peak:
+                curr_peak = curr_profit
+            else:
+                curr_peak = prev_peak
+            cum_profit_data.loc[cum_profit_data.index[row], 'Peak_' + symbol] = curr_peak
+            curr_dd = (curr_profit - curr_peak)/curr_peak*100
+            cum_profit_data.loc[cum_profit_data.index[row], 'DD%_' + symbol] = curr_dd
+
+    #         # If previous profit is 0 then change is 0
+    #         elif cum_profit_data.iloc[row - 1, column] == 0:
+    #             cum_profit_data.loc[cum_profit_data.index[row], 'Change%_'+symbol] = 0
+    #         else:
+    #             # Change in %
+    #             change = ((cum_profit_data.iloc[row, column] - cum_profit_data.iloc[row - 1][column]) / cum_profit_data.iloc[row - 1, column] * 100)
+    #             # If new profit is higher the previous, change is positive, else is negative
+    #             curr_profit = cum_profit_data.iloc[row , column]
+    #             prev_profit = cum_profit_data.iloc[row - 1, column]
+    #             if curr_profit > prev_profit:
+    #                 change = abs(change)
+    #             else:
+    #                 change = -abs(change)
+    #             cum_profit_data.loc[cum_profit_data.index[row], 'Change%_'+symbol] = change
+    #
+    # # Change for summary
+    # column = cum_profit_data.columns.get_loc('Sum profit')
+    #
+    # # Pass each row- index
+    # for row in number_of_days:
+    #     # Skip first row (no previous profit)
+    #     if row == 0:
+    #         pass
+    #     # If previous profit is 0 then change is 0
+    #     elif cum_profit_data.iloc[row - 1, column] == 0:
+    #         cum_profit_data.loc[cum_profit_data.index[row], 'Change%_sum'] = 0
+    #     else:
+    #         # Change in %
+    #         change = ((cum_profit_data.iloc[row, column] - cum_profit_data.iloc[row - 1][column]) /
+    #                   cum_profit_data.iloc[row - 1, column] * 100)
+    #         # If new profit is higher the previous, change is positive, else is negative
+    #         curr_profit = cum_profit_data.iloc[row, column]
+    #         prev_profit = cum_profit_data.iloc[row - 1, column]
+    #         if curr_profit > prev_profit:
+    #             change = abs(change)
+    #         else:
+    #             change = -abs(change)
+    #         cum_profit_data.loc[cum_profit_data.index[row], 'Change%_sum'] = change
+    #
+    #
+
+    cum_profit_data = cum_profit_data.round(2)
 
     return cum_profit_data
 
@@ -154,6 +228,7 @@ def backtest_summary(fdata):
 if __name__ == '__main__':
 
     backtest = backtest_atr_weight(symbols, test_period_y, atr_time_period, account)
+    # backtest_summary(backtest)
     print(backtest_summary(backtest))
 
     # output = current_atr_weight(symbols, atr_time_period, initial_account)
